bd298a8aef14594697a0932c74374ca360a7344a
Jon Phoenix <jonathan.martb@gmail.com>
2020-06-28 04:39:02 -0700

    Variants and loop for password result

--- js/script.js
+++ js/script.js
@@ -4,17 +4,19 @@ var generatePassword = document.querySelector("#generate-button").addEventListen
 var copyPassword = document.querySelector("#copy-button").addEventListener("click", copyPassword);
 
 /* Array Declaration */
-var numChar = ["0,1,2,3,4,5,6,7,8,9"];
-var specChar = ["!,@,#,%,^,&,*,<,>,?,~,/,+,-,(,):,;,[,],_,{,},$"];
-var lowerCChar = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
-var upperCChar = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
+var numCharArr = ["0","1","2","3","4","5","6","7","8","9"];
+var speciCharArr = ["!","@","#","%","^","&","*","<",">","?","~","/","+","-","(",")",":",";","[","]","_","{","}","$"];
+var lowerCharArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCharArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 /* Variable declaration */
 var confirmLength
 var confirmNumChar;
-var confirmSpecChar;
-var confirmLowerCCase;
-var confirmUpperCCase;
+var confirmSpeciChar;
+var confirmLowerChar;
+var confirmUpperChar;
+var passwordChar;
+var passwordResult;
 
 /* Function to generate password. Step 1: Confirming the number of characters */
 function generatePassword() {
@@ -26,12 +28,48 @@ function generatePassword() {
         var confirmLength = prompt("Choose your password length between 8 and 129 characters:");
     } else {alert(`Your password will have ${confirmLength} characters`);}
 
+    console.log(confirmLength)
+
+    var confirmNumChar = confirm("Click OK if you want to include numeric characters");
+    var confirmSpeciChar = confirm("Click OK if you want to include special characters");
+    var confirmLowerChar = confirm("Click OK if you want to include lower case characters");
+    var confirmUpperChar = confirm("Click OK if you want to include upper case characters");
+
+    if (confirmNumChar === false && confirmSpeciChar === false && confirmLowerChar === false && confirmUpperChar === false) {
+        alert("You must choose at least one option. Try again");
+        var confirmNumChar = confirm("Click OK if you want to include numeric characters");
+        var confirmSpeciChar = confirm("Click OK if you want to include special characters");
+        var confirmLowerChar = confirm("Click OK if you want to include lower case characters");
+        var confirmUpperChar = confirm("Click OK if you want to include upper case characters");
+    } else {var passwordChar = []}
+
+    if (confirmNumChar) {
+        passwordChar = passwordChar.concat(numCharArr);
+    }
+
+    if (confirmSpeciChar) {
+        passwordChar = passwordChar.concat(speciCharArr);
+    }
+
+    if (confirmLowerChar) {
+        passwordChar = passwordChar.concat(lowerCharArr);
+    }
+
+    if (confirmUpperChar) {
+        passwordChar = passwordChar.concat(upperCharArr);
+    }
+    
+    console.log(passwordChar)
+
+    var passwordResult = ""
+
+    for (var i = 0; i < confirmLength; i++) {
+        passwordResult = passwordResult + passwordChar[Math.floor(Math.random() * passwordChar.length)];
+        console.log(passwordResult)
+    }
+    return passwordResult;
 }
 
 function copyPassword() {
     alert("Your password was copied to your clipboard")
-}
-
-for (var i = 0; i < confirmLength; i++) {
-
 }
