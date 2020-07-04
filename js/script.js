d677e1e410f2fb85da32c825ea1522389a90f382
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-04 01:32:02 -0700

    Updating Arrays

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@
 document.querySelector('#generate-button').addEventListener('click', writePassword);
 
 // Arrays declaration four groups of characters
-var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var speArr = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var lowArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 // Creating a fifth empty array to combine four arrays
 var passChar = [];
 
