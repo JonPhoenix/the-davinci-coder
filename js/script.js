64b6417b932da6c2d3917e230a63e17bf370523e
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-05 16:15:11 -0700

    Updating for loop

--- js/script.js
+++ js/script.js
@@ -1,35 +1,23 @@
 // Building the Password generator script clean slate
 document.querySelector('#generate-button').addEventListener('click', writePassword);
-
 // Arrays declaration four groups of characters
 var numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var speArr = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var lowArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Creating a fifth empty array to combine four arrays
-var passChar = [];
-
-//.Concat password Characters and arrays
-passChar = passChar.concat(numArr);
-passChar = passChar.concat(speArr);
-passChar = passChar.concat(lowArr);
-passChar = passChar.concat(uppArr);
-
-console.log(passChar)
-
-//function 1: Parameters for password length and loops until true
+// Function 1: Parameters for password length and loops until true
 function generatePassword() {
-
+    // Creating a fifth empty array to combine four arrays
+    var passChar = [];
+    var randomPass = [];
+	var passResult = [];
     var passLength = parseInt(prompt('Choose your password length between 8 and 129 characters:'));
 
     if (passLength < 8 || passLength > 129) {
         return alert('You must choose between 8 and 129 characters. Try again.');
     }
-
     alert(`Your password will have ${passLength} characters`);
-
-    console.log(passLength)
 
     var confirmNum = confirm('Click OK if you want to include numeric characters.');
     var confirmSpe = confirm('Click OK if you want to include special characters.');
@@ -39,7 +27,6 @@ function generatePassword() {
     if (!confirmNum && !confirmSpe && !confirmLow && !confirmUpp) {
         return alert('You must include at least one group of characters. Try again');
     }
-
     var optUser = {
         passLength: passLength,
         confirmNum: confirmNum,
@@ -48,10 +35,7 @@ function generatePassword() {
         confirmUpp: confirmUpp,
     }
 
-    console.log(optUser)
-
-    var passChar = [];
-
+    //.Concat password characters and arrays. User decide how big the PassChar array is
     if (optUser.confirmNum === true) {
         passChar = passChar.concat(numArr);
     }
@@ -64,19 +48,14 @@ function generatePassword() {
     if (optUser.confirmUpp === true) {
         passChar = passChar.concat(uppArr);
     }
-
-    console.log(passChar)
-
-    var randomPass = [];
-    var passResult = [];
-
+	// The for loop will happen as many times as user said they wanted length of password
     for (var i = 0; i < optUser.passLength; i++) { 
-        var randomPass = Math.floor(Math.random() * optUser.passLength); 
+
+        var randomPass = Math.floor(Math.random() * passChar.length); 
         passResult.push(passChar[randomPass]);
     }
 
     var newPassword = passResult.join('');
-
     return newPassword;
 }
 
